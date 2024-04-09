55d2cf9d1b1b9cb814065f5acb7786eddb2133ac
matthew ledford <95671719+mled-v@users.noreply.github.com>
2024-04-09 16:25:57 -0500

    Add files via upload

--- sort_characters_by_frequency_451.py
+++ sort_characters_by_frequency_451.py
@@ -38,8 +38,6 @@ def frequencySort(s):
         else:
             my_dict[i] += 1
 
-
-
     sorted_dict = dict(sorted(my_dict.items(), key=lambda x:x[1], reverse=True))
 
     for key, value in sorted_dict.items():
